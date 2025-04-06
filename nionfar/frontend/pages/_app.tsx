70fb079b62192fc8fdd4f3b9eab04fcfd995f69e
ABG <badzagueye@gmail.com>
2025-04-06 19:49:45 +0200

    Fix: Résolution des problèmes de runtime JSX avec configuration Babel optimisée

--- nionfar/frontend/pages/_app.tsx
+++ nionfar/frontend/pages/_app.tsx
@@ -1,5 +1,5 @@
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
-import { useEffect } from 'react';
 import Head from 'next/head';
 import { AuthProvider } from '../contexts/AuthContext';
 import 'tailwindcss/tailwind.css';
@@ -16,7 +16,7 @@ const SafeHydrate = ({ children }) => {
   );
 };
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   // Mettre à jour le titre de la page uniquement côté client
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -30,25 +30,29 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <SafeHydrate>
-      <AuthProvider>
-        <Head>
-          <title>Nionfar.sn | La plateforme de freelance au Sénégal</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <Component {...pageProps} />
-        <ToastContainer 
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </AuthProvider>
-    </SafeHydrate>
+    <React.Fragment>
+      <SafeHydrate>
+        <AuthProvider>
+          <Head>
+            <title>Nionfar.sn | La plateforme de freelance au Sénégal</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <Component {...pageProps} />
+          <ToastContainer 
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </AuthProvider>
+      </SafeHydrate>
+    </React.Fragment>
   );
 }
+
+export default MyApp;
--- nionfar/frontend/pages/_document.tsx
+++ nionfar/frontend/pages/_document.tsx
@@ -1,18 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
-export default function Document() {
-  return (
-    <Html lang="fr">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
