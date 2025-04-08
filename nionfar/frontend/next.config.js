bc5db996f694503c7dbe06513d6d4447e75840c7
ABG <badzagueye@gmail.com>
2025-04-08 09:16:02 +0200

    Fix API rewrites in next.config.js and vercel.json

--- nionfar/frontend/next.config.js
+++ nionfar/frontend/next.config.js
@@ -121,7 +121,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL + '/:path*',
+        destination: (process.env.NEXT_PUBLIC_API_URL || 'https://nionfar-backend.onrender.com/api') + '/:path*',
       },
     ];
   },
