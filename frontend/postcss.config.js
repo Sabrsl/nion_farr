2aee6a327575067b8bda83164d3b7f7f9acbcb74
ABG <badzagueye@gmail.com>
2025-04-09 04:24:36 +0200

    Fix tailwind import in globals.css

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-nesting': {},
-    'tailwindcss': {},
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 } 
